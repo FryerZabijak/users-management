6d01c2fd03a52eaef73f4787b774b0e85787c3f9
Pepa Mráz <pepamraz1@gmail.com>
2023-01-22 00:30:59 +0100

    Final 1.0 Flask, Routing, Templates, CSS, Basic functions

---
+++ hello.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+@app.route('/user/<int:user_id>')
+def user(user_id):
+    user = User.query.get(user_id)
+    return render_template('user.html', user=user)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        user = User(username, email)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('users'))
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
